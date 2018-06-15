04d1f717e58a72e29b967e939b5ae9e08c94a6af
Drey Tee <audreyteteh7@gmail.com>
2018-06-14 22:07:02 -0500

    Activity Switcher first trial

---
+++ app/src/main/java/com/drey/activityswitcher/MainActivity.java
@@ -0,0 +1,19 @@
+package com.drey.activityswitcher;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void onClickSwitchActivity(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
