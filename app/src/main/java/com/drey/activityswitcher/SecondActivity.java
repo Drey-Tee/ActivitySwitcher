44bf486f651a2745f10a0ed91f948bc8563a47a2
Drey Tee <audreyteteh7@gmail.com>
2018-06-20 22:39:09 -0500

    Returning a Result from an Activity

--- app/src/main/java/com/drey/activityswitcher/SecondActivity.java
+++ app/src/main/java/com/drey/activityswitcher/SecondActivity.java
@@ -1,16 +1,24 @@
 package com.drey.activityswitcher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 public void onClickClose(View view){
+    Intent returnIntent = new Intent();
+    returnIntent.putExtra(MainActivity.REQUEST_RESULT,42);
+    setResult(RESULT_OK, returnIntent);
     finish();
 }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView textView = (TextView) findViewById(
+                R.id.textViewText);
+        if (getIntent()!=null && getIntent().hasExtra(Intent.EXTRA_TEXT));
     }
 }
